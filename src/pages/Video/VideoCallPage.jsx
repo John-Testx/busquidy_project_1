b119c6b8214a3ef9fecd061ee79411f8b980956a
John-Testx <juan.astudillomurua@gmail.com>
2025-12-01 00:25:08 -0300

    fixes video_server_url xd

--- src/pages/Video/VideoCallPage.jsx
+++ src/pages/Video/VideoCallPage.jsx
@@ -8,7 +8,7 @@ import {
     FaPhoneSlash, FaDesktop, FaPaperPlane, FaComment, FaCommentSlash
 } from 'react-icons/fa';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL_2;
 
 const VideoCallPage = () => {
     const { roomId } = useParams();
